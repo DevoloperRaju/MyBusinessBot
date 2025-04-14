c47a6b49c22ff177235292ed79f1bb643d9a5e59
DevoloperRaju <106170865+DevoloperRaju@users.noreply.github.com>
2025-04-14 21:29:58 +0600

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,5 +1,18 @@
+const mainMenu = require('./main_menu');
+
 module.exports = (bot) => {
   bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Hi! I am alive.");
+    const chatId = msg.chat.id;
+
+    const welcomeMessage = `
+Welcome to our bot!
+Here you will find many exciting options.
+Please select from the menu below.
+    `;
+
+    bot.sendMessage(chatId, welcomeMessage.trim())
+      .then(() => {
+        mainMenu(bot, chatId);
+      });
   });
 };
