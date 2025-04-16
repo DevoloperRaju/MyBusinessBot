3ab8953f9daa74ff04c4325c6d77e2608d086b50
DevoloperRaju <106170865+DevoloperRaju@users.noreply.github.com>
2025-04-16 13:39:13 +0600

    Update dailyReward.js

--- menus/dailyReward.js
+++ menus/dailyReward.js
@@ -1,1 +1,16 @@
-ji
+module.exports = (bot, chatId) => {
+  const menu = {
+    reply_markup: {
+      keyboard: [
+        ['🎁 G', '🕑 H'],
+        ['📢 N', '👥 O'],
+        ['💰 S', '💎 S'],
+        ['🧩 L', '📢 P'],
+        ['⬅️ Previous Page', '🏅 L']
+      ],
+      resize_keyboard: true
+    }
+  };
+
+  bot.sendMessage(chatId, 'Welcome to the Reward World! Please choose an option below:', menu);
+};
