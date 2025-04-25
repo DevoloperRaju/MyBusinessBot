70192c615294798f0af759a6eeef9290cbd6e804
DevoloperRaju <106170865+DevoloperRaju@users.noreply.github.com>
2025-04-25 11:03:47 +0600

    Update profile.js

--- menus/profile.js
+++ menus/profile.js
@@ -1,14 +1,14 @@
 module.exports = (bot, chatId) => {
-const menu = {
-reply_markup: {
-keyboard: [
-['ℹ️ Set Wallet', 'ℹ️ Set FB'],
-['ℹ️'Set Area, 'ℹ️ Set etc'],
-['➡️ Next Page']
-],
-resize_keyboard: true
-}
-};
+  const menu = {
+    reply_markup: {
+      keyboard: [
+        ['ℹ️ Set Wallet', 'ℹ️ Set FB'],
+        ['ℹ️ Set Area', 'ℹ️ Set etc'],
+        ['➡️ Next Page']
+      ],
+      resize_keyboard: true
+    }
+  };
 
-bot.sendMessage(chatId, 'Welcome to the Main Menu! Please choose an option below:', menu);
+  bot.sendMessage(chatId, 'Welcome to the Main Menu! Please choose an option below:', menu);
 };
